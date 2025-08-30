685a35c2a7c3d1a787c926174c6c7265976d528d
ariba <ariba2576@gmail.com>
2025-08-30 14:53:05 +0300

    hm4

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,90 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //1 task
+        byte age = 17;
+        if (age >=18){
+            System.out.println("Если возраст человека равен "+ age +" , то он совершеннолетний");
         }
+        else{
+            System.out.println(" «Если возраст человека равен "+ age + " , то он не достиг совершеннолетия, нужно немного подождать».");
+        }
+        //2 task
+        byte temp = 15;
+        if (temp<5){
+            System.out.println("На улице "+temp+" градусов, нужно надеть шапку");
+        }
+        if (temp>5){
+            System.out.println("На улице "+temp+" градусов, можно идти без шапки");
+        }
+        //3 task
+        short speed = 70;
+        boolean SpeedMoreThan60 = (speed >60);
+        if(SpeedMoreThan60){
+            System.out.println("Если скорость "+speed+" , то придется заплатить штраф.");
+        }else{
+            System.out.println("Если скорость "+speed+" , то придется заплатить штраф.");
+        }
+        //4 task
+        byte ag = 10;
+        if( 2<= ag && ag <=6){
+            System.out.println("Если возраст человека равен "+ag+" , то ему нужно ходить в детский сад" );
+        }else if (7<=ag && ag <=17){
+            System.out.println("Если возраст человека равен "+ag+" , то ему нужно ходить в школу" );
+        }else if (18<=ag && ag <=24){
+            System.out.println("Если возраст человека равен "+ag+" , то ему нужно ходить в университет" );
+        }else if (25<=ag){
+            System.out.println("Если возраст человека равен "+ag+" , то ему пора ходить на работу" );
+        }
+        //5 task
+        ag = 17;
+        if(ag <=5){
+            System.out.println("Если возраст ребенка равен "+ag +", то ему нельзя кататься на аттракционе " );
+        }else if (6<=ag && ag <14){
+            System.out.println("Если возраст ребенка равен "+ag +", то ему можно кататься на аттракционе в сопровождении" );
+        }else if (14<=ag){
+            System.out.println("Если возраст ребенка равен "+ag +", то ему можно кататься на аттракционе без сопровождения");
+        }
+        //6 task
+        byte kolPass = 54;
+        if (kolPass <60){
+            System.out.println("Есть и сидячие, и стоячие места");
+        }else if (kolPass >=60 && kolPass <102){
+            System.out.println("Есть только стоячие места");
+        }else{
+            System.out.println("Вагон уже полностью забит.");
+        }
+        //7 task при условии что все числа различны
+        int one =5;
+        int two=6;
+        int three=7;
+        if (one<two){
+            if (two<three){
+                System.out.println("Three is the biggest");
+            }
+            else if (two>three) {
+                System.out.println("Two is the biggest");
+            }
+        }
+        else if(one>two){
+            if (one>three){
+                System.out.println("One is the biggest");
+            }else if (three> one){
+                System.out.println("Three is the biggest");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
